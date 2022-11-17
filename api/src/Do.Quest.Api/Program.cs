e2b35fb4829880939a8f6b26c1691424951df0ac
Guilherme Edinger <gedinger@gvdasa.com.br>
2022-11-17 00:40:18 -0300

    Libera Cors

--- api/src/Do.Quest.Api/Program.cs
+++ api/src/Do.Quest.Api/Program.cs
@@ -22,14 +22,21 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 builder.Services.AddScoped<QuestionarioContext>();
 
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+    {
+      builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+    }));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
+
+app.UseCors("corsapp");
 
 app.UseHttpsRedirection();
 
